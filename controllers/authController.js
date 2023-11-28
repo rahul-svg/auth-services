01e2593f2f7ac6eeed473c7326b20a725db85c2a
Rahul Kumar <rahul.gadi@50hertz.in>
2023-11-28 11:48:40 +0530

    Auth Login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,35 @@
-const User = require('../models/authUser')
+const User = require('../models/authUser');
+const jwt = require('jsonwebtoken');
+
+// create Token
+const maxAge = 3000 * 24 * 60 * 60 
+const createToken = (id) => {
+  return jwt.sign({id},'food-delivery',{
+    expiresIn:maxAge
+  })
+}
+
+// handle errors
+const handleErrors = (err) => {
+  console.log(err.message, err.code);
+  let errors = { email: '', password: '' };
+
+  // duplicate email error
+  if (err.code === 11000) {
+    errors.email = 'that email is already registered';
+    return errors;
+  }
+
+  // validation errors
+  if (err.message.includes('user validation failed')) {
+    Object.values(err.errors).forEach(({ properties }) => {
+      errors[properties.path] = properties.message;
+    });
+  }
+
+  return errors;
+}
+
 
 // controller actions
 module.exports.signup_get = (req, res) => {
@@ -17,9 +48,12 @@ module.exports.signup_get = (req, res) => {
    const {email,password} = req.body;
    try{
       const user = await User.create({email,password})
-      res.status(201).json(user)
+      const token = createToken(user._id);
+      res.cookie('jwt',token,{httpOnly:true,maxAge : maxAge * 1000});
+      res.status(201).json({user : user._id})
    }catch(err){
-    console.log(err)
+    const errors = handleErrors(err);
+    res.status(400).json({ errors });
    }
   }
   
--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-const express = require('express')
+const express = require('express');
 const dotenv = require('dotenv');
 require('dotenv').config();
 const connectDB = require('./config/db');
 const PORT = process.env.PORT || 5000;
 const mongoURI = process.env.MONGO_URI
 const app = express();
-const authRoutes = require('./routes/authRoutes')
+const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
 
-// Middlewares
+//  Middlewares
 app.use(express.json());
 app.use('/auth-services',authRoutes);
+app.use(cookieParser());
 
 // DB Connect
-connectDB(mongoURI);
-
+ connectDB(mongoURI);
+ 
 app.listen(PORT,() => {
     console.log(`App is running at ${PORT}`)
 })
