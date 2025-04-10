bcf27b05522f8fa2479e707697241c3bd459c0cf
rahul-svg <rahulgadi21111999@gmail.com>
2025-04-10 13:32:22 +0530

    loign issue fixed

--- models/authUser.js
+++ models/authUser.js
@@ -3,13 +3,6 @@ const { isEmail } = require('validator');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-   email: {
-    type: String,
-    required: [true, 'Please enter an email'],
-    unique: true,
-    lowercase: true,
-    validate: [isEmail, 'Please enter a valid email']
-  },
   email: {
     type: String,
     required: [true, 'Please enter an email'],
@@ -43,6 +36,7 @@ const userSchema = new mongoose.Schema({
 
 // fire a function before doc saved to db
 userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next(); 
   const salt = await bcrypt.genSalt();
   this.password = await bcrypt.hash(this.password, salt);
   next();
